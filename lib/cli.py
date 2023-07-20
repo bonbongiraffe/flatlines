7e6681244f8454ac05395826c69bcfd7f9abd34a
bonbongiraffe <francescowai@yahoo.com>
2023-07-20 18:23:12 -0400

    pilot functions

--- lib/cli.py
+++ lib/cli.py
@@ -12,13 +12,13 @@ def main_menu(*args):
     menu_dict = {
             "c": create_reservation,
             "e": edit_reservation,
-            "p": register_pilot
+            "p": pilot_register_login
         }
     print("""
         Please select from the following menu options:
         c - create a new reservation
         e - edit an existing reservation
-        p - register as new pilot
+        p - pilot register/login
         x - exit 
         """)
     while True:
@@ -123,7 +123,7 @@ def create_reservation():
             print("Exiting application. Hope to see you again soon!")
             break
 
-#FIND existing reservation:
+#FIND existing reservation
 def find_reservation():
     #prompts user for reservation number and returns reservation object once found
     while True:
@@ -138,7 +138,7 @@ def find_reservation():
             print("Reservation not found. Please enter a valid reservation number.")
     return search_reservation
 
-#CHANGE seat:
+#CHANGE seat
 def change_seat(existing_reservation):
     current_reservation = existing_reservation
     current_flight = existing_reservation.flight
@@ -164,7 +164,7 @@ def change_seat(existing_reservation):
             print("Seat number updated successfully")
             main_menu()
 
-#CANCEL reservation:
+#CANCEL reservation
 def cancel_reservation(existing_reservation):
     while True:
         confirmation = input('Are you sure you want to cancel this reservation ? (y/n): ')
@@ -217,46 +217,205 @@ def edit_reservation(existing_reservation=None):
         else:
             print("Invalid menu option. Please try again.")
 
-#REGISTER new pilot
-def register_pilot():
+#PILOT password check
+def pilot_password():
     incorrect_pass_count = 0
     while True:
         print("Please enter the secret password.")
         user_input = input(">> ")
         if user_input == 'const [torture, setTorture] = useState(SqlAlchemy)':
-            print ("Welcome to Flatlines!")
-            while True:
-                first_name = input("Please enter your first name: ")
-                if validate_name(first_name):
-                    first_name = first_name.rstrip().lower()
-                    break
-            while True:
-                last_name = input("Please enter your last name: ")
-                if validate_name(last_name):
-                    last_name = last_name.rstrip().lower()
-                    break
-            while True:
-                id = input("Enter id number: ")
-                if id.isdigit():
-                    if int(id) not in Pilot.ids():
-                        break
-                    else:
-                        print(f"{id} is already taken.")
-                else:
-                    print("id must be a number.")
-            new_pilot = Pilot(first_name = first_name, last_name = last_name, id = int(id))
-            session.add(new_pilot)
-            session.commit()
-            print("Successfully registerd new pilot.")
-            main_menu()
+            print ("\nFlatline Meow Potato!")
+            return True
+        elif incorrect_pass_count == 2:
+            print("You've been ejected. Exiting application.")
+            return False
+        else:
+            incorrect_pass_count += 1
+            print(f"Incorrect password. {3-incorrect_pass_count} attempts remaining.\n")
+
+#PILOT print flight seating chart and passenger list
+def pilot_print_flight_info(logged_in_flight):
+    print(logged_in_flight.seat_chart)
+    print(logged_in_flight.passenger_list)
+
+#PILOT eject passenger from flight
+def pilot_cancel_reservation(logged_in_flight):
+    flight = logged_in_flight
+    print(flight.passenger_list)
+    if flight.passenger_ids == []:
+        print("No passengers to eject.")
+        return
+    while True:
+        print("Please enter a passenger to eject: ")
+        user_input = input(">> ")
+        if user_input.isdigit():
+            if int(user_input) in flight.passenger_ids:
+                current_reservation = session.query(Reservation).filter_by(passenger_id=int(user_input),flight_id=flight.id).first()
+                print(f"First matching reservation for {current_reservation.passenger.name} on flight {flight.id} will be deleted.")
+                session.delete(current_reservation)
+                session.commit()
+                break
+            else:
+                print("Passenger id not found on flight.")
+        else:
+            print("Passenger id must be a number.")
+
+#PILOT edit flight
+def pilot_edit_flight(logged_in_pilot, flight_id):
+    pilot = logged_in_pilot
+    flight = session.query(Flight).filter_by(id=flight_id).first()
+    print(flight)
+    menu_dict = {
+        "s": pilot_print_flight_info,
+        "e": pilot_cancel_reservation,
+    }
+    while True:
+        print("""
+        Please select from the following menu options:
+        s - print flight seat chart and passenger list
+        e - eject passenger from flight
+        x - return to previous menu
+        """)
+        user_input = input(">> ")
+        if user_input in menu_dict:
+            menu_dict[user_input](flight)
+        elif user_input == 'x':
+            print("Exiting application. Hope to see you again soon!")
+            break
+        else:
+            print("Invalid menu option. Please try again.")
+
+#PILOT select flight
+def pilot_select_flight(logged_in_pilot):
+    pilot = logged_in_pilot
+    while True:
+        print("Please enter a flight id: ")
+        input_id = input(">> ")
+        if input_id.isdigit():
+            if int(input_id) in pilot.flight_ids:
+                pilot_edit_flight(pilot, int(input_id))
+                break
+            else:
+                print("Flight id is piloted by another pilot. Please select from your own flights.")
+        else: 
+            print("Invalid flight id. Please try again.")
+
+#PILOT print flights
+def pilot_print_flights(logged_in_pilot):
+    print(logged_in_pilot.flight_list)
+
+#PILOT menu
+def pilot_menu(logged_in_pilot):
+    pilot = logged_in_pilot
+    print(f"Welcome back, {pilot.name}!")
+    menu_dict = {
+        "f": pilot_print_flights,
+        "e": pilot_select_flight,
+        "m": main_menu
+    }
+    while True:
+        print("""
+    Please select from the following menu options:
+    f - print list of your flights
+    e - edit flight
+    x - return to previous menu
+    """)
+        user_input = input(">> ")
+        if user_input in menu_dict:
+            menu_dict[user_input](pilot)
+        elif user_input == 'x':
+            print("Exiting application. Hope to see you again soon!")
+            break
+        else:
+            print("Invalid menu option. Please try again.")
+
+#PILOT login
+def pilot_login():
+    incorrect_pass_count = 0
+    while True:
+        print("Please enter your first name: ")
+        first_name = input(">> ")
+        print("Please enter your last name: ")
+        last_name = input(">> ")
+        print("Please enter your pilot id: ")
+        input_id = input(">> ")
+        checks = []
+        for pilot in session.query(Pilot):
+            first_name_check = first_name == pilot.first_name
+            last_name_check = last_name == pilot.last_name
+            id_check = input_id == str(pilot.id)
+            if first_name_check and last_name_check and id_check:
+                checks.append(True)
+            else:
+                checks.append(False)
+            # print(pilot)
+            # print(first_name_check, last_name_check, id_check)
+        if True in checks:
+            current_pilot = session.query(Pilot).filter_by(id=input_id).first()
+            pilot_menu(current_pilot)
             break
         elif incorrect_pass_count == 2:
             print("You've been ejected. Exiting application.")
             break
         else:
             incorrect_pass_count += 1
-            print(f"Incorrect password. {3-incorrect_pass_count} attempts remaining.")
+            print(f"Invalid credentials. {3-incorrect_pass_count} attempts remaining.")
+                
+#PILOT register/login
+def pilot_register_login():
+    if pilot_password() == False:
+        return
+    print("""
+    Please select from the following menu options:
+    r - register as new pilot
+    l - login for existing pilots
+    m - main menu
+    x - exit application
+    """)
+    menu_dict = {
+        "r": register_pilot,
+        "l": pilot_login,
+        "m": main_menu
+    }
+    while True:
+        user_input = input(">> ")
+        if user_input in menu_dict.keys():
+            menu_dict[user_input]()
+            break
+        elif user_input == "x":
+            print("Exiting application. Hope to see you again soon!")
+            break
+        else:
+            print("Invalid menu option. Please try again.")
 
+#PILOT register new pilot
+def register_pilot():
+    while True:
+        first_name = input("Please enter your first name: ")
+        if validate_name(first_name):
+            first_name = first_name.rstrip().lower()
+            break
+    while True:
+        last_name = input("Please enter your last name: ")
+        if validate_name(last_name):
+            last_name = last_name.rstrip().lower()
+            break
+    while True:
+        id = input("Enter id number: ")
+        if id.isdigit():
+            if int(id) not in Pilot.ids():
+                break
+            else:
+                print(f"{id} is already taken.")
+        else:
+            print("id must be a number.")
+    new_pilot = Pilot(first_name = first_name, last_name = last_name, id = int(id))
+    session.add(new_pilot)
+    session.commit()
+    print("\nSuccessfully registered new pilot.")
+    main_menu()
+
+#________________________________________
 #CLI
 if __name__ == '__main__':
     print(f"""
